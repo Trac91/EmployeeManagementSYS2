97f3ffe3534c80642ccad2d6a0a85253cf1a1e25
t00202341 <Tracey.brosnan@students.ittralee.ie>
2018-11-12 16:01:37 +0000

    Make Mortgage Application Form Fix event handling

--- MenuJFrame.java
+++ MenuJFrame.java
@@ -13,7 +13,7 @@ public class MenuJFrame implements ActionListener {
     JMenu file,employees,mortgages;
     JMenuItem exit, addEmployee, editEmployee, deleteEmployee, applyMortgage,viewMortgages;
     JButton addEmployeeButton;
-    private Container contentPane;
+  //  private Container contentPane;
 
 
     public MenuJFrame() {
@@ -30,13 +30,17 @@ public class MenuJFrame implements ActionListener {
         JMenu employees = new JMenu("Employees");
         JMenu mortgages = new JMenu("Mortgages");
         JMenuItem exit = new JMenuItem("Exit");
-        exit.addActionListener(this);
+            exit.addActionListener(this);
         JMenuItem addEmployee = new JMenuItem("Add Employee");
-        addEmployee.addActionListener(this);
+            addEmployee.addActionListener(this);
         JMenuItem editEmployee = new JMenuItem("Edit Employee");
+            editEmployee.addActionListener(this);
         JMenuItem deleteEmployee = new JMenuItem("Delete Employee");
+            deleteEmployee.addActionListener(this);
         JMenuItem applyMortgage = new JMenuItem("Apply");
+            applyMortgage.addActionListener(this);
         JMenuItem viewMortgage = new JMenuItem("View Applications");
+            viewMortgage.addActionListener(this);
 
         menuBar.add(file);
         menuBar.add(employees);
@@ -63,133 +67,145 @@ public class MenuJFrame implements ActionListener {
             employeeForm.setVisible(true);
             employeeForm.validate();
         }
+        else if(e.getActionCommand().equals("Edit Employee"))
+        {
+            // search bar appears to find employee to edit
+            // employee form appears with fields filled with employee details ready to be edited
+            employeeForm.setVisible(true);
+        }
+
+        else if(e.getActionCommand().equals("Delete Employee"))
+        {
+            // search bar to find employee to be deleted
+            // employee form appears with fields filled with employee details ready to be edited
+            // Delete option with confirmation message
+        }
+        else if(e.getActionCommand().equals("Apply"))
+        {
+            // Application form appears
+            //details will be saved to file
+        }
+
+        else if(e.getActionCommand().equals("View Applications"))
+        {
+            //JTable with Mortgage details appear showing the status of the application
+            // Options to cancel
+        }
     }
 
+    public class EmployeeForm
+    {
 
-              public class MouseListenerForMenuItems extends MouseAdapter
-            {
-                public void mouseClicked(MouseEvent e)
-                {
-                    if(e.getSource() == addEmployee)
-                    {
-                        // EmployeeForm JPanel
-                        JPanel employeeForm = new JPanel(new GridBagLayout()); // Employee details panel
-                        employeeForm.setSize(500,600);
-                        employeeForm.setBackground(Color.lightGray);
-                        employeeForm.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK,1,true),"Register Employee"));
-                        JFrameWindow.add(employeeForm);
+    }
 
-                        // Set Position of components
-                        GridBagConstraints c = new GridBagConstraints();
-                        c.gridx = 0; // Referenced from Mr Java Help Youtube
-                        c.gridy = 0;
-                        c.anchor = GridBagConstraints.LINE_START;
-                        c.insets = new Insets(10,20,5,20);
+    public void EmployeeForm()
+    {
+        // EmployeeForm JPanel
+        JPanel employeeForm = new JPanel(new GridBagLayout()); // Employee details panel
+        employeeForm.setSize(500,600);
+        employeeForm.setBackground(Color.lightGray);
+        employeeForm.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK,1,true),"Register Employee"));
+        JFrameWindow.add(employeeForm);
 
-                        //JLabels for Employee Form
-                        JLabel idLabel = new JLabel("Employee ID");
-                        employeeForm.add(idLabel,c);
-                        c.gridy++; // Increments and moves to row below
+        // Set Position of components
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0; // Referenced from Mr Java Help Youtube
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.insets = new Insets(10,20,5,20);
 
-                        JLabel nameLabel = new JLabel("\nName\n");
-                        employeeForm.add(nameLabel,c);
-                        c.gridy++;
+        //JLabels for Employee Form
+        JLabel idLabel = new JLabel("Employee ID");
+        employeeForm.add(idLabel,c);
+        c.gridy++; // Increments and moves to row below
 
-                        JLabel addressLabel = new JLabel("\nAddress\n");
-                        employeeForm.add(addressLabel,c);
-                        c.gridy++;
+        JLabel nameLabel = new JLabel("\nName\n");
+        employeeForm.add(nameLabel,c);
+        c.gridy++;
 
-                        JLabel emailLabel = new JLabel("\nEmail");
-                        employeeForm.add(emailLabel,c);
-                        c.gridy++;
+        JLabel addressLabel = new JLabel("\nAddress\n");
+        employeeForm.add(addressLabel,c);
+        c.gridy++;
 
-                        JLabel phoneLabel = new JLabel("\nPhone No.");
-                        employeeForm.add(phoneLabel,c);
-                        c.gridy++;
+        JLabel emailLabel = new JLabel("\nEmail");
+        employeeForm.add(emailLabel,c);
+        c.gridy++;
 
-                        JLabel departmentLabel = new JLabel("Department");
-                        employeeForm.add(departmentLabel,c);
-                        c.gridy++;
+        JLabel phoneLabel = new JLabel("\nPhone No.");
+        employeeForm.add(phoneLabel,c);
+        c.gridy++;
 
-                        JLabel jobTitleLabel = new JLabel("Title");
-                        employeeForm.add(jobTitleLabel,c);
-                        c.gridy++;
+        JLabel departmentLabel = new JLabel("Department");
+        employeeForm.add(departmentLabel,c);
+        c.gridy++;
 
-                        c.gridx=1;
-                        c.gridy=0;
-                        c.anchor = GridBagConstraints.LINE_START;
+        JLabel jobTitleLabel = new JLabel("Title");
+        employeeForm.add(jobTitleLabel,c);
+        c.gridy++;
 
-                        // Creates fields/combobox for employee form
+        c.gridx=1;
+        c.gridy=0;
+        c.anchor = GridBagConstraints.LINE_START;
 
-                        JTextField idField = new JTextField(5);
-                        employeeForm.add(idField,c);
-                        c.gridy++;
+        // Creates fields/combobox for employee form
 
-                        JTextField nameField = new JTextField(20);
-                        employeeForm.add(nameField,c);
-                        c.gridy++;
+        JTextField idField = new JTextField(5);
+        employeeForm.add(idField,c);
+        c.gridy++;
 
-                        JTextArea addressField = new JTextArea(5,20);
-                        employeeForm.add(addressField,c);
-                        c.gridy++;
+        JTextField nameField = new JTextField(20);
+        employeeForm.add(nameField,c);
+        c.gridy++;
 
-                        JTextField emailField = new JTextField(20);
-                        employeeForm.add(emailField,c);
-                        c.gridy++;
+        JTextArea addressField = new JTextArea(5,20);
+        employeeForm.add(addressField,c);
+        c.gridy++;
 
-                        JTextField phoneField = new JTextField(15);
-                        employeeForm.add(phoneField,c);
-                        c.gridy++;
+        JTextField emailField = new JTextField(20);
+        employeeForm.add(emailField,c);
+        c.gridy++;
 
-                        // Create array for departments
-                        String [] departments = {"Accounts", "Mortgages", "Front Desk", "Marketing"};
-                        // Create combo box
-                        JComboBox departmentField = new JComboBox(departments);
-                        employeeForm.add(departmentField,c);
-                        c.gridy++;
+        JTextField phoneField = new JTextField(15);
+        employeeForm.add(phoneField,c);
+        c.gridy++;
 
-                        // Create arrays for Job Titles
-                        String [] jobTitles = {"Branch Manager","Accounts Manager", "Mortgage Manager", "General Manager", "Marketing Manager", "Admin", "Mortgage Consultants", "Bank Teller","Marketing Consultants"};
+        // Create array for departments
+        String [] departments = {"Accounts", "Mortgages", "Front Desk", "Marketing"};
+        // Create combo box
+        JComboBox departmentField = new JComboBox(departments);
+        employeeForm.add(departmentField,c);
+        c.gridy++;
 
-                        JComboBox divisionField = new JComboBox(jobTitles);
-                        employeeForm.add(divisionField,c);
-                        c.gridy++;
+        // Create arrays for Job Titles
+        String [] jobTitles = {"Branch Manager","Accounts Manager", "Mortgage Manager", "General Manager", "Marketing Manager", "Admin", "Mortgage Consultants", "Bank Teller","Marketing Consultants"};
 
-                        // Create Clear/ Register button for employee form
-                        JButton clearButton = new JButton("Clear");
-                        c.gridx = 0;
-                        c.gridy = 7;
-                        c.anchor = GridBagConstraints.LINE_END;
-                        c.insets = new Insets(20,20,20,20);
-                        employeeForm.add(clearButton,c);
+        JComboBox divisionField = new JComboBox(jobTitles);
+        employeeForm.add(divisionField,c);
+        c.gridy++;
 
-                        JButton registerButton = new JButton("Register Employee");
-                        c.gridx = 1;
-                        c.gridy = 7;
-                        c.anchor = GridBagConstraints.LINE_END;
-                        employeeForm.add(registerButton,c);
+        // Create Clear/ Register button for employee form
+        JButton clearButton = new JButton("Clear");
+        c.gridx = 0;
+        c.gridy = 7;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.insets = new Insets(20,20,20,20);
+        employeeForm.add(clearButton,c);
 
-                        JFrameWindow.add(employeeForm);
+        JButton registerButton = new JButton("Register Employee");
+        c.gridx = 1;
+        c.gridy = 7;
+        c.anchor = GridBagConstraints.LINE_END;
+        employeeForm.add(registerButton,c);
 
-                        employeeForm.setVisible(false);
+        JFrameWindow.add(employeeForm);
 
-
-
-                    }
-
-                    class formAction implements ActionListener
-                    {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                        }
-                    }
-
-                }
+        //employeeForm.setVisible(false);
 
 
+    }
 
-            }
-        }
+}
+
 
 
 
