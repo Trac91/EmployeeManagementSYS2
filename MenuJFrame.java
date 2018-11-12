91fa57407135d14c012d863f09f9a406a4eaefbc
t00202341 <Tracey.brosnan@students.ittralee.ie>
2018-11-12 21:44:32 +0000

    Fix closing operations for JFrame

--- MenuJFrame.java
+++ MenuJFrame.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MenuJFrame implements ActionListener {
@@ -17,7 +19,7 @@ public class MenuJFrame implements ActionListener {
         JFrameWindow = new JFrame();
         JFrameWindow.setVisible(true);
         JFrameWindow.setTitle("Employee Management System");
-        JFrameWindow.setSize(1500, 1000);
+        JFrameWindow.setSize(2000, 1500);
         JFrameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Menu Bar
@@ -48,6 +50,7 @@ public class MenuJFrame implements ActionListener {
         employees.add(deleteEmployee);
         mortgages.add(applyMortgage);
         mortgages.add(viewMortgage);
+        JFrameWindow.setVisible(true);
     }
 
     @Override
@@ -64,6 +67,7 @@ public class MenuJFrame implements ActionListener {
         }
         else if(e.getActionCommand().equals("Edit Employee"))
         {
+            JFrameWindow.revalidate();
             EmployeeForm();
             searchPanel();
             // search bar appears to find employee to edit
@@ -72,12 +76,17 @@ public class MenuJFrame implements ActionListener {
 
         else if(e.getActionCommand().equals("Delete Employee"))
         {
+            JFrameWindow.revalidate();
+            searchPanel();
+
             // search bar to find employee to be deleted
             // employee form appears with fields filled with employee details ready to be edited
             // Delete option with confirmation message
         }
         else if(e.getActionCommand().equals("Apply"))
         {
+            JFrameWindow.revalidate();
+            mortgageForm();
             // Application form appears
             //details will be saved to file
         }
@@ -103,6 +112,7 @@ public class MenuJFrame implements ActionListener {
         JOptionPane.showMessageDialog(null,"Loading Employee panel");
         // EmployeeForm JPanel
         JPanel employeeForm = new JPanel(new GridBagLayout()); // Employee details panel
+
         employeeForm.setSize(500,600);
         employeeForm.setBackground(Color.lightGray);
         employeeForm.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK,1,true),"Employee Details"));
@@ -199,8 +209,8 @@ public class MenuJFrame implements ActionListener {
 
         Container pane = JFrameWindow.getContentPane();
         pane.add(employeeForm);
+        JFrameWindow.setVisible(true);
     }
-
     public class searchPanel
     {
         JFrame JFrameWindow;
@@ -213,13 +223,14 @@ public class MenuJFrame implements ActionListener {
     {
         JOptionPane.showMessageDialog(null,"Loading search panels");
         JPanel searchPanel = new JPanel(new GridBagLayout());
-
         searchPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK,1,true),"Search Employee ID / Name"));
         searchPanel.setSize(100,200);
-
-
-        searchPanel.setBackground(Color.cyan);
+        searchPanel.setBounds(12,12,100,100);
+        searchPanel.setVisible(true);
         GridBagConstraints c1 = new GridBagConstraints();
+        c1.anchor = GridBagConstraints.LINE_START;
+        c1.insets = new Insets(20,20,20,20);
+        searchPanel.setBackground(Color.cyan);
         c1.gridx =0;
         c1.gridy=0;
 
@@ -238,10 +249,110 @@ public class MenuJFrame implements ActionListener {
         searchPanel.add(searchField,c1); // add search field on search panel
          searchPanel.add(searchLabel,c1);
         searchPanel.add(searchButton,c1); // add search button to search panel
-        searchPanel.setVisible(true);
-
+        //searchPanel.setVisible(true);
         Container pane = JFrameWindow.getContentPane();
         pane.add(searchPanel);
+        JFrameWindow.setVisible(true);
+    }
+
+    public class mortgageForm
+    {
+        JPanel mortgageForm;
+
+        JComboBox numApplicantsBox, statusBox, firstTimeBox, numChildrenBox;
+
+         JLabel numApplicants, fName, sName, status, dob,numChildren, firstTimeBuyer,
+             income , additionalIncome, childcare, maintenence,price,loanRequired;
+
+         JTextField fNameField, sNameField, dobField, incomeField, additionalIncomeField,
+             childcareField, mainteneceField, priceField, loanRequiredField;
+    }
+
+    public void mortgageForm()
+
+    {
+        JPanel mortgagePanel = new JPanel(new GridBagLayout());
+        mortgagePanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK,1,true),"Mortgage Application"));
+        mortgagePanel.setSize(100,200);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx =0;
+        c.gridy=0;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.insets = new Insets(10,20,10,20);
+
+        // create labels
+        JLabel numApplicants = new JLabel("No. of applicants");
+        JLabel status = new JLabel("Marital status");
+        JLabel fName = new JLabel("Forename");
+        JLabel sName = new JLabel("Surname");
+        JLabel dob = new JLabel("Date of birth");
+        JLabel numChildren = new JLabel("No. of dependant children");
+        JLabel firstTimeBuyer = new JLabel("First time buyer");
+        JLabel income = new JLabel("Basic income");
+        JLabel additionalIncome = new JLabel("Additional Income");
+        JLabel childcare = new JLabel("Childcare costs");
+        JLabel maintenance = new JLabel("Maintenance costs");
+        JLabel price = new JLabel("Purchase Price");
+        JLabel loanRequired = new JLabel("Mortgage loan required");
+
+        //create fields
+        String [] numApp = {"1", "2"};
+        JComboBox numApplicantsCombo = new JComboBox(numApp);
+        String [] statusArray = {"Single", "Married", "Divorced/Separated", "Widowed", "Cohabitant"};
+        JComboBox statusCombo = new JComboBox(statusArray);
+        JTextField fNameField = new JTextField(20);
+        JTextField sNameField = new JTextField(20);
+        // date picker here
+
+        String [] numChildrenArray = {"None","1", "2", "3", "4", "5", "6", "7", "8", "9+"};
+        JComboBox numChildrenCombo = new JComboBox(numChildrenArray);
+        String [] options = {"Yes", "No"};
+        JComboBox firstTimeBuyerCombo = new JComboBox(options);
+        JTextField incomeField = new JTextField(20);
+        JTextField additionalIncomeField = new JTextField(20);
+        JTextField childcareField = new JTextField(20);
+        JTextField maintenanceField = new JTextField(20);
+        JTextField priceField = new JTextField(20);
+        JTextField loanRequiredField = new JTextField(20);
+
+        //add labels
+        mortgagePanel.add(numApplicants,c); c.gridy++;
+        mortgagePanel.add(fName,c); c.gridy++;
+        mortgagePanel.add(sName,c); c.gridy++;
+        mortgagePanel.add(status,c); c.gridy++;
+        mortgagePanel.add(dob,c); c.gridy++;
+        mortgagePanel.add(numChildren,c); c.gridy++;
+        mortgagePanel.add(firstTimeBuyer,c); c.gridy++;
+        mortgagePanel.add(income,c); c.gridy++;
+        mortgagePanel.add(additionalIncome,c); c.gridy++;
+        mortgagePanel.add(childcare,c); c.gridy++;
+        mortgagePanel.add(maintenance,c); c.gridy++;
+        mortgagePanel.add(price,c); c.gridy++;
+        mortgagePanel.add(loanRequired,c); c.gridy++;
+
+        //addfields
+        c.gridx = 1;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_END;
+        mortgagePanel.add(numApplicantsCombo,c);c.gridy++;
+        mortgagePanel.add(fNameField,c);c.gridy++;
+        mortgagePanel.add(sNameField,c);c.gridy++;
+        mortgagePanel.add(statusCombo,c);c.gridy++;
+        mortgagePanel.add(dob,c);c.gridy++;
+        mortgagePanel.add(numChildrenCombo,c);c.gridy++;
+        mortgagePanel.add(firstTimeBuyerCombo,c);c.gridy++;
+        mortgagePanel.add(incomeField,c);c.gridy++;
+        mortgagePanel.add(additionalIncomeField,c);c.gridy++;
+        mortgagePanel.add(childcareField,c);c.gridy++;
+        mortgagePanel.add(maintenanceField,c);c.gridy++;
+        mortgagePanel.add(priceField,c);c.gridy++;
+        mortgagePanel.add(loanRequiredField,c);c.gridy++;
+
+        mortgagePanel.setVisible(true);
+        Container pane = JFrameWindow.getContentPane();
+        pane.add(mortgagePanel);
+
+        JFrameWindow.setVisible(true);
 
 
 
@@ -251,9 +362,7 @@ public class MenuJFrame implements ActionListener {
 
 
 
-
-
-
+//https://stackoverflow.com/questions/8770617/java-items-appear-only-after-the-window-is-resize
 
 
 
